a266c4daa83832178c6e602537f2231ce9fb330d
ThansudaJ21 <thansuda_janthakham.cmu.ac.th>
2021-08-09 19:33:55 +0700

    Create card to show name and status of vaccine

---
+++ src/components/Personcard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="q-pa-md row items-start q-gutter-md">
+    <q-card class="my-card" >
+      <img src="https://cdn.quasar.dev/img/mountains.jpg">
+
+      <q-card-section>
+        <div class="text-h6">Name: </div>
+        <div class="text-subtitle2">Status: first dose</div>
+      </q-card-section>
+    </q-card>
+  </div>
+</template>
+
+<style>
+.my-card{
+width: 100%;
+  max-width: 250px;
+ color: white;
+border-radius: 30px !important;
+background-color: #68b2a0 !important; 
+text-align: center;
+}
+  
+</style>
+
+<script>
+export default {
+  name: "Personcard",
+};
+</script>
