cfd0244bdaefb23d792a7d70d6368caaa4474f13
ThansudaJ21 <thansuda_janthakham.cmu.ac.th>
2021-08-10 20:34:28 +0700

    Update router and fix cinflicts

--- src/router/index.js
+++ src/router/index.js
@@ -9,24 +9,6 @@ const routes = [{
     props: (route) => ({ page: parseInt(route.query.page) || 1 })
 },
 {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-        import( /* webpackChunkName: "about" */ "../views/About.vue"),
-},
-  path: "/",
-  name: "Home",
-  component: Home,
-}, {
-  path: '/personList',
-  name: 'PersonList',
-  component: PersonList,
-  props: (route) => ({ page: parseInt(route.query.page) || 1 })
-},
-{
   path: "/about",
   name: "About",
   // route level code-splitting
