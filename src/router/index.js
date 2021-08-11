a25e6f2fbff11e59436c735502475ee147b0045c
NNilawan <nilawan_l@cmu.ac.th>
2021-08-11 21:42:43 +0700

    Add DoctorDetails path

--- src/router/index.js
+++ src/router/index.js
@@ -1,52 +1,60 @@
 import { createRouter, createWebHistory } from "vue-router";
 import PersonList from '../views/PersonList.vue'
 import PersonDetails from '../views/Person/PersonDetails.vue'
+import VaccineDetails from '../views/Person/VaccineDetails.vue'
+import DoctorDetails from '../views/Person/DoctorDetails.vue'
 import NProgress from 'nprogress'
 import NotFound from '@/views/NotFound.vue'
 import NetWorkError from '@/views/NetworkError.vue'
 const routes = [{
-    path: '/',
-    name: 'PersonList',
-    component: PersonList,
-    props: (route) => ({ page: parseInt(route.query.page) || 1 })
-},
-{
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-        import( /* webpackChunkName: "about" */ "../views/About.vue"),
-},
-{
-    path: "/datas/:id",
-    name: "PersonDetails",
-    component: PersonDetails,
-    props: true
-},
-{
-    path: '/datas/:id/vaccine-details',
-    name: 'VaccineDetails',
-    component: VaccineDetails,
-    props: true
-},
-{
-    path: '/404/:resource',
-    name: '404Resource',
-    component: NotFound,
-    props: true
-},
-{
-    path: '/:catchAll(.*)',
-    name: 'NotFound',
-    component: NotFound
-},
-{
-    path: '/network-error',
-    name: 'NetworkError',
-    component: NetWorkError
-}
+        path: '/',
+        name: 'PersonList',
+        component: PersonList,
+        props: (route) => ({ page: parseInt(route.query.page) || 1 })
+    },
+    {
+        path: "/about",
+        name: "About",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ "../views/About.vue"),
+    },
+    {
+        path: "/datas/:id",
+        name: "PersonDetails",
+        component: PersonDetails,
+        props: true
+    },
+    {
+        path: '/datas/:id/vaccine-details',
+        name: 'VaccineDetails',
+        component: VaccineDetails,
+        props: true
+    },
+    {
+        path: '/datas/:id/doctor-details',
+        name: 'DoctorDetails',
+        component: DoctorDetails,
+        props: true
+    },
+    {
+        path: '/404/:resource',
+        name: '404Resource',
+        component: NotFound,
+        props: true
+    },
+    {
+        path: '/:catchAll(.*)',
+        name: 'NotFound',
+        component: NotFound
+    },
+    {
+        path: '/network-error',
+        name: 'NetworkError',
+        component: NetWorkError
+    }
 
 ];
 
